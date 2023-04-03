bbee5d693fe1bfefee8b407bf93443646a5e4150
Muhammad Faliqh Yoon <faliqhyoon@outlook.com>
2023-04-03 11:02:17 +0700

    Initial commit

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Faliqh (itsfaliqhh)
+// | @author_url 1: http://www.github.com/iamfaliqhh
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: faliqhyoon@outlook.com
+// +------------------------------------------------------------------------+
+// | Muzzer Stream - The Music Streaming Platform
+// | Copyright (c) 2023 Muzzer. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "muzzerstream";
+
+// Site URL
+$site_url = "http://localhost/Muzzer"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "0"; // Your purchase code, don't give it to anyone.
+?>
